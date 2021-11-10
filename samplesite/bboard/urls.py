6827a196c27d681b8060d56f99ffd6ca76ac83c0
zhenerBY <zhener@tut.by>
2021-11-10 17:17:22 +0300

    ch2 done

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric, BbCreateView
 
+# ZHE сылки в прямом виде
+# urlpatterns = [
+#     path('<int:rubric_id>/', by_rubric),
+#     path('', index),
+# ]
+
+# ZHE именованные маршруты
 urlpatterns = [
-    path('', index),
+    path('/add', BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,6 +1,53 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Bb
+from .models import Rubric
+from .forms import BbForm
 
 
-def index(reqeust):
-    return HttpResponse('Здесь будет вывыеден список объявлений.')
+# ZHE вывыдим просто текст
+# def index(request):
+#     s = 'Список объявлений\n\n\n'
+#     for bb in Bb.objects.order_by('-published'):
+#         s += bb.title + '\n' + bb.content + '\n\n'
+#     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+# ZHE рендерим низкоуровневыми инструментами
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+# ZHE рендер на основе сокращений
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
+
+# ZHE вывод рубрик (1,2 и т.д.)
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    # success_url = '/bboard/'
+    #ZHE не указывать путь прямым способом
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
